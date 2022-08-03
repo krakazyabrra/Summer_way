e96a8777cf25bcc3c21d3dcec0a37d7d54e22c26
krakazyabrra <norman63194@gmail.com>
2022-08-03 22:55:29 +0300

    I gat how to make it work

---
+++ Simple Tic-Tac-Toe/Topics/Standard input/Four numbers/src/Main.kt
@@ -0,0 +1,13 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+
+    val num1 = scanner.nextInt()
+    val num2 = scanner.nextInt()
+    val num3 = scanner.nextInt()
+    val num4 = scanner.nextInt()
+
+    println("$num1\n$num2\n$num3\n$num4")
+}
---
+++ Simple Tic-Tac-Toe/Topics/Standard input/Read and print/src/Main.kt
@@ -0,0 +1,11 @@
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    // put your code here
+    val word1 = readln()
+    val word2 = readln()
+    val (num_1, num_2) = readln().split(' ')
+
+    println("$word1\n$word2\n$num_1\n$num_2")
+}
